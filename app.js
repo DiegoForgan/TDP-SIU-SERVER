ad404c166e1ab01d6dcec86917efb3e4b668dbc9
DiegoForgan <dmforgan89@gmail.com>
2018-09-15 05:11:03 -0300

    agrega correctamente a la base y cambia metodo de configuracion del pool

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const db = require('./db');
 const PORT = process.env.PORT || 8080;
 
 app.get('/base', (req, res) => {
-    var resp = (db.query('INSERT INTO padron(dni, apellido, nombre) VALUES($1, $2, $3) RETURNING *', ['12345678', 'carlson', 'brian'], function(){}));
+    var resp = (db.query('INSERT INTO padron(dni, apellido, nombre) VALUES($1, $2, $3) RETURNING *', ['87654321', 'martinez', 'mariano'], function(){}));
     res.send('Ejecucion OK');
 });
 
--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
 
+//Aqui hay que ver como se llama la variable de ambiente de heroku pero mientras tanto esta hardcodeado el string de mi base de pruebas
+const string_de_coneccion = process.env.connectionString || 'postgresql://postgres:12345@localhost:5432/test';
+
+//Se cambia a esta forma de configuracion que es mas adaptable a ser incorporada a Heroku
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'test',
-    password: '12345',
-    port: 5432,
-})
+    connectionString: string_de_coneccion,
+});
 
 module.exports = {
   query: (text, params, callback) => {
