74c7a6066cb188e5c65ed0814609729accc984a2
DiegoForgan <dmforgan89@gmail.com>
2018-09-15 22:58:47 -0300

    salida genera Json con prioridad

--- routes/alumno.js
+++ routes/alumno.js
@@ -1,6 +1,12 @@
 var router = require('express').Router();
 var db = require('../db');
 
+function crearJSON(prioridad_del_alumno){
+    var salida = {'prioridad':'' };
+    salida.prioridad = prioridad_del_alumno;
+    return salida;
+}
+
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
     console.log('Middleware says %s %s', req.method, req.url);
@@ -18,7 +24,7 @@ router.get('/prioridad/:padron', function(req, res) {
     //aca iria el codigo con la query a la tabla de alumnos sobre su prioridad
     var prioridad_del_alumno = Math.floor((Math.random() * 100) + 1); //esto calcula una prioridad random entre 1 y 100 de forma provisoria
     var string_de_respuesta = "Tu padron es: " + padron_del_alumno + " y tu prioridad es: " + prioridad_del_alumno;
-    res.send(string_de_respuesta);
+    res.send(crearJSON(prioridad_del_alumno));
 });
   
 module.exports = router;
