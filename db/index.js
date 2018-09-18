9b30a1dcae3750af70dfeeacb908a3886a9e88be
Agustin <agus.luques@hotmail.com>
2018-09-18 13:12:34 -0300

    creo tabla docentes y la pueblo con datos

--- db/index.js
+++ db/index.js
@@ -23,7 +23,21 @@ pool.query("DROP TABLE IF EXISTS alumnos;\
 			insert into alumnos values('96803', 'luques', 'agustin','agusluques', 'contrasecreta', 1);\
 			insert into alumnos values('10101', 'riquelme', 'juan roman','romi', 'capo', 10);\
 			\
-            ",null,(req,res)=>{console.log(res)});
+            ");
+
+pool.query("DROP TABLE IF EXISTS docentes;\
+			\
+			create table docentes(\
+				legajo varchar(10) not null,\
+				apellido varchar(200) not null,\
+				nombre varchar(200) not null,\
+				usuario varchar(50) not null,\
+				contrasena varchar(20) not null);\
+			\
+			insert into docentes values('12345', 'fontela', 'carlos','calitos10', 'tdplove');\
+			insert into docentes values('00000', 'calonico', 'cristian','calo', 'pass123');\
+			\
+            ")
 
 module.exports = {
   query: (text, params, callback) => {
