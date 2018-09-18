0233da349994aea0eea47f7ec15ffc9cf2a3382f
Agustin <agus.luques@hotmail.com>
2018-09-18 13:07:52 -0300

    creo tabla alumnos y la pueblo con datos

--- db/index.js
+++ db/index.js
@@ -10,6 +10,21 @@ const pool = new Pool({
     max: 1, //lo limito a 1 por temas de sincronismo pero lo ideal del pool seria trabajar con multiples
 });
 
+pool.query("DROP TABLE IF EXISTS alumnos;\
+			\
+			create table alumnos(\
+				padron varchar(10) not null,\
+				apellido varchar(200) not null,\
+				nombre varchar(200) not null,\
+				usuario varchar(50) not null,\
+				contrasena varchar(20) not null,\
+				prioridad int not null);\
+			\
+			insert into alumnos values('96803', 'luques', 'agustin','agusluques', 'contrasecreta', 1);\
+			insert into alumnos values('10101', 'riquelme', 'juan roman','romi', 'capo', 10);\
+			\
+            ",null,(req,res)=>{console.log(res)});
+
 module.exports = {
   query: (text, params, callback) => {
     return pool.query(text, params, callback);
