0c44fc5800f9a1f4c4db66e71d77e9c979936ca7
DiegoForgan <dmforgan89@gmail.com>
2018-09-21 20:03:01 -0300

    Devuelve la prioridad dado un padron, sino devuelve undefined

--- routes/alumno.js
+++ routes/alumno.js
@@ -1,17 +1,9 @@
 var router = require('express').Router();
 var db = require('../db');
 
-function crearJSON(prioridad_del_alumno) {
-    var salida = {
-        'prioridad': ''
-    };
-    salida.prioridad = prioridad_del_alumno;
-    return salida;
-}
-
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
-    console.log('Middleware says %s %s', req.method, req.url);
+    console.log('Se solicito %s en el endpoint /alumno %s', req.method, req.url);
     next();
 });
 
@@ -21,12 +13,21 @@ router.get('/', function (req, res) {
 });
 
 // ruta para consultar prioridad del alumno con su padron
+// Si el padron del alumno existe en la base de datos, devuelve un JSON con el campo prioridad y su valor
+// Si el padron NO EXISTE en la base de datos, devuelve un JSON con el campo prioridad y valor "undefined"
 router.get('/prioridad/:padron', function (req, res) {
     var padron_del_alumno = req.params.padron;
     console.log('Estas buscando la prioridad del alumno cuyo padron es: ' + padron_del_alumno);
-    
     db.query('SELECT prioridad FROM alumnos WHERE padron = $1', [padron_del_alumno], (error, respuesta) => {
-        if (!error) res.send(respuesta.rows[0]);
+        if (!error) {
+            if (respuesta.rowCount != 0) {
+                res.send(respuesta.rows[0]);
+            } else {
+                res.send({
+                    "prioridad": "undefined"
+                });
+            }
+        }
     });
 });
   
