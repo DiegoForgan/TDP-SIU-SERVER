203b4fca75964c99053dd0fb787f2584e7da2845
DiegoForgan <dmforgan89@gmail.com>
2018-09-21 19:21:11 -0300

    Devuelve prioridad. Revisar alumnos que no estan en la base.

--- routes/alumno.js
+++ routes/alumno.js
@@ -1,8 +1,10 @@
 var router = require('express').Router();
 var db = require('../db');
 
-function crearJSON(prioridad_del_alumno){
-    var salida = {'prioridad':'' };
+function crearJSON(prioridad_del_alumno) {
+    var salida = {
+        'prioridad': ''
+    };
     salida.prioridad = prioridad_del_alumno;
     return salida;
 }
@@ -14,17 +16,18 @@ router.use(function timeLog(req, res, next) {
 });
 
 // ruta base de alumno
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.send('Bienvenido alumno!');
 });
 
 // ruta para consultar prioridad del alumno con su padron
-router.get('/prioridad/:padron', function(req, res) {
+router.get('/prioridad/:padron', function (req, res) {
     var padron_del_alumno = req.params.padron;
-    //aca iria el codigo con la query a la tabla de alumnos sobre su prioridad
-    var prioridad_del_alumno = Math.floor((Math.random() * 100) + 1); //esto calcula una prioridad random entre 1 y 100 de forma provisoria
-    var string_de_respuesta = "Tu padron es: " + padron_del_alumno + " y tu prioridad es: " + prioridad_del_alumno;
-    res.send(crearJSON(prioridad_del_alumno));
+    console.log('Estas buscando la prioridad del alumno cuyo padron es: ' + padron_del_alumno);
+    
+    db.query('SELECT prioridad FROM alumnos WHERE padron = $1', [padron_del_alumno], (error, respuesta) => {
+        if (!error) res.send(respuesta.rows[0]);
+    });
 });
   
 module.exports = router;
