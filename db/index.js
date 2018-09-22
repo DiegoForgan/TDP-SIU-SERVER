3ec5e58ee97b7c30415e900cf05ae13691349635
DiegoForgan <dmforgan89@gmail.com>
2018-09-21 22:15:11 -0300

    Formato de base de CURSOS. Inserto dos de ejemplo.

--- db/index.js
+++ db/index.js
@@ -37,7 +37,30 @@ pool.query("DROP TABLE IF EXISTS docentes;\
 			insert into docentes values('12345', 'fontela', 'carlos','calitos10', 'tdplove');\
 			insert into docentes values('00000', 'calonico', 'cristian','calo', 'pass123');\
 			\
-            ")
+			");
+// formatos definidos para la tabla:
+// docente_a_cargo: contiene el numero de legajo del docente a cargo (por ahora se considera UNICO)
+// sede: Se completa PC o LH para cada dia de cursada. Por ej: "PC-LH" (un dia en cada sede);
+// aula: numero de 3 cifras o laboratorio designado
+// dias: se coloca cada dia sepado por coma (,)
+// horario: se coloca la franja como "HH-HH;HH-HH" 
+pool.query("DROP TABLE IF EXISTS cursos;\
+			\
+			create table cursos(\
+				id_curso SERIAL PRIMARY KEY,\
+				codigo varchar(6) not null,\
+				nombre varchar(40) not null,\
+				docente_a_cargo varchar(10) not null,\
+				sede varchar(10) not null,\
+				aula varchar(10) not null,\
+				cupos_disponibles int not null,\
+				dias varchar(40) not null,\
+				horario varchar(40) not null);\
+			\
+			insert into cursos values(DEFAULT,'75.46','Taller de desarrollo de proyectos II','12345','PC','201',30,'lunes','17-22');\
+			insert into cursos values(DEFAULT,'75.73','Arquitectura de Software','00000','LAB B',20,'jueves','18-22');\
+			\
+			");
 
 module.exports = {
   query: (text, params, callback) => {
