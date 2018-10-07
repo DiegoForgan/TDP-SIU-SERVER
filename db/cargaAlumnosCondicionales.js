4e174f0cd986224b222958c90b8c16db93876fd8
DiegoForgan <dmforgan89@gmail.com>
2018-10-07 04:44:25 -0300

    elimino campos ahora innecesarios

--- db/cargaAlumnosCondicionales.js
+++ db/cargaAlumnosCondicionales.js
@@ -4,9 +4,6 @@ module.exports = function(pool){
 			INSERT INTO inscripciones VALUES('00002',14, true);\
 			INSERT INTO inscripciones VALUES('00003',14, true);\
             INSERT INTO inscripciones VALUES('00004',14, false);\
-            INSERT INTO inscripciones VALUES('00005',14, false);\
-            UPDATE cursos\
-            SET inscriptos = 3, cupos_disponibles = 0, condicionales = 2\
-            WHERE cursos.id_curso = 14;"
+            INSERT INTO inscripciones VALUES('00005',14, false);"
         );
 }
