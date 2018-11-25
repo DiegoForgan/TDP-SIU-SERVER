a12c6dcce7f528ab28b9f8b9dacbed358e580fe3
Agustin <agus.luques@hotmail.com>
2018-11-25 14:18:25 -0300

    fix bugs

--- routes/admin.js
+++ routes/admin.js
@@ -161,7 +161,7 @@ router.post('/periodos', (req, res) => {
 });
 
 router.post('/notificaciones', (req, res) => {
-    var titulo = new Date().toLocaleString() + " - " + req.body.titulo;
+    var titulo = req.body.titulo;
     var texto = req.body.texto;
 
     try {
