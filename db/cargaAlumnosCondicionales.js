fba0de4297adfd14ac01eb91f9d1a6e9fc09067c
DiegoForgan <dmforgan89@gmail.com>
2018-12-03 16:53:24 -0300

    fix mascherano y borro alumnos de admin 1

--- db/cargaAlumnosCondicionales.js
+++ db/cargaAlumnosCondicionales.js
@@ -6,14 +6,10 @@ module.exports = function(pool){
             INSERT INTO inscripciones VALUES('00004',14, false);\
             INSERT INTO inscripciones VALUES('00005',14, false);\
             INSERT INTO inscripciones VALUES('95812',14, false);\
-            INSERT INTO inscripciones VALUES('00001',5, true);\
-			INSERT INTO inscripciones VALUES('00002',5, true);\
 			INSERT INTO inscripciones VALUES('00003',6, true);\
             INSERT INTO inscripciones VALUES('00004',4, true);\
             INSERT INTO inscripciones VALUES('00005',4, true);\
-			INSERT INTO inscripciones VALUES('00006',5, true);\
 			INSERT INTO inscripciones VALUES('00007',6, true);\
-            INSERT INTO inscripciones VALUES('00008',4, true);\
             INSERT INTO inscripciones VALUES('00008',4, true);\
 			INSERT INTO inscripciones VALUES('00010',4, true);\
             INSERT INTO inscripciones VALUES('95812',4, true);\
